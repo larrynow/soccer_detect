b8eb0c13f944fb8e72cd02dbc9ccd3dc7030d276
larrynow <llfwyyx@163.com>
2017-06-10 21:42:58 +0800

    A update for saving images in order.

--- frame_extract.py
+++ frame_extract.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from PIL import Image
 import cv2
-
+import os
 
 #class frame_extract:
 
@@ -9,7 +9,12 @@ import cv2
     #def slice(dir):
 
 if __name__=='__main__':
-    vc = cv2.VideoCapture('test.mp4') #读入视频文件
+    gamename = 'rm-atm'
+    if os.path.exists('imgs/%s'%gamename):
+        pass
+    else:
+        os.makedirs('imgs/%s'%gamename)
+    vc = cv2.VideoCapture('videos/%s.mp4'%gamename) #读入视频文件
     c=1
 
     if vc.isOpened(): #判断是否正常打开
@@ -22,7 +27,7 @@ if __name__=='__main__':
     while rval:   #循环读取视频帧
         rval, frame = vc.read()
         if(c%timeF == 0): #每隔timeF帧进行存储操作
-            cv2.imwrite('pic/'+str(c) + '.jpg',frame) #存储为图像
+            cv2.imwrite('imgs/%s/%s'%(gamename,gamename)+str(c) + '.jpg',frame) #存储为图像
         c = c + 1
         cv2.waitKey(1)
     vc.release()
--- image_slicer.py
+++ image_slicer.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from PIL import Image
 import numpy as np
+import os
 
 class image_slicer:
     #define the size,step of a slide window:
@@ -11,12 +12,19 @@ class image_slicer:
 
     #choose which img to slice:
     def slice(self,img,img_name):
+        os.makedirs('imgs/%s_sliced'%img_name)
         for i in range(int(img.size[0]/self.step)):
             for j in range(int(img.size[1]/self.step)):
                 img_sliced = img.crop((i*self.step,j*self.step,i*self.step+self.width,j*self.step+self.length))
-                img_array = np.array(img_sliced.convert('L'))
-                print(np.shape(img_array))
-                img_sliced.convert('L').save("pic/%s_sliced%d-%d.jpg"%(img_name,i,j))
+                self.process_img(img_sliced).save("imgs/%s_sliced/%s[%d,%d].jpg"%(img_name,img_name,i,j))
+                #save
+                #img_sliced.convert('L').save("pic/%s_sliced%d-%d.jpg"%(img_name,i,j))
+
+    #to process img:
+    def process_img(self,img):
+        #convert pics to 'L' mode
+        img_processed = img.convert('L')
+        return img_processed
 
 '''if __name__=="__main__":
     img = Image.open("C:/Users/龙飞/PycharmProjects/soccer_detect/pic/test.jpg")
@@ -26,6 +34,6 @@ class image_slicer:
 '''
 if __name__=="__main__":
 
-    img = Image.open("C:/Users/龙飞/PycharmProjects/soccer_detect/pic/test.jpg")
+    img = Image.open("C:/Users/龙飞/PycharmProjects/soccer_detect/imgs/test.jpg")
     image_slicer = image_slicer(300,300,300)
     image_slicer.slice(img,'test')
